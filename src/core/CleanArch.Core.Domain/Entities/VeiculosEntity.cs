7a2e6167de7878afb4384aa81bc577fece8ab80f
Victor Hugo da Silva <victor.silva_ext@interplayers.com.br>
2023-11-25 11:39:13 -0300

    #Criando entidade de Veiculos

---
+++ src/core/CleanArch.Core.Domain/Entities/VeiculosEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace CleanArch.Core.Domain.Entities
+{
+    public class VeiculosEntity : AudityEntity
+    {
+        public string Marca { get; private set; }
+
+        public string Modelo { get; private set; }
+
+        public string Cor { get; private set; }
+
+        public string Placa { get; private set; }
+
+        public int Tipo { get; private set; }
+
+        public VeiculosEntity() { }
+
+        public VeiculosEntity(string marca,
+            string modelo,
+            string cor,
+            string placa,
+            int tipo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+            Placa = placa;
+            Tipo = tipo;
+        }
+    }
+}
